d76d16f83472ea066bbe68f5578fc9342e250c07
Zain Badshah <zainbadshah786@hotmail.com>
2023-01-30 12:45:21 +0000

    user info

---
+++ user_details.py
@@ -0,0 +1,13 @@
+# Details
+name = input("What is your name? ")
+age = input("What is your age? ")
+shoesize = input("What is your  shoesize? ")
+birthday = input("When is your birthday? ")
+
+print(f"Hello {name.capitalize()}, you are {age} years old. Your birthday is {birthday} and you are {float(shoesize):.1f} cm tall ")
+
+# Address Details
+Door_number = input("Door number? ")
+postcode = input("Postcode? ")
+
+print(f"Your address: {Door_number}  {postcode}")
